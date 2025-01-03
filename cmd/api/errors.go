5ddbb34c8cff5c006f6d3d934f7668c303b56111
Hazel Granados <hazel.granados@protonmail.com>
2025-01-03 13:06:14 -0600

    🥅 Internal errors package

---
+++ cmd/api/errors.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	writeJSONError(w, http.StatusNotFound, "resource not found")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
